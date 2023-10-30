5144ce2ae94f6d2a3bbb677f85ce9b40866a65b7
AlexTouchedGrass <dean-potts.622237@swl.k12.oh.us>
2023-10-30 09:28:34 -0400

    Losing my mind

--- src/LoopAsCounter.java
+++ src/LoopAsCounter.java
@@ -1,5 +1,24 @@
-public class Main {
+import java.util.Scanner;
+import java.lang.Math;
+
+public class LoopAsCounter {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        for (int i = 0; i < 31; i++) {
+            System.out.print(" "+i);
+        }
+        System.out.println("");
+        for (int j = 30; j >= 0; j--){
+            System.out.print(" "+j);
+        }
+        System.out.println("");
+        for (int v = 0; v <= 18; v = v+3){
+            System.out.print(" "+v);
+        }
+        System.out.println("");
+        for (int x = 10; x >= 0; x = x-2) {
+            System.out.print(" " + x);
+        }
     }
 }
